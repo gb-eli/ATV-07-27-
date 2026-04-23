c37edf016a24fc8937fd2e8d62f53905e1334baf
Gabriel Elienai <162762339+gb-eli@users.noreply.github.com>
2026-04-23 15:17:00 -0300

    Add files via upload

---
+++ ex11.py
@@ -0,0 +1,9 @@
+# Exercício 11 - Número par ou ímpar
+numero = int(input("Digite um número: "))
+
+if numero % 2 == 0:
+    print("Número par")
+else:
+    print("Número ímpar")
+
+# explicação encerrou no 11 para o DS3 07-04 12:49
